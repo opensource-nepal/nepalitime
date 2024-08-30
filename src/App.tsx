a5b5ca43456864af4c95be5a8eb6a5f243ed6efe
Ajesh Sen Thapa <aj3sshh@gmail.com>
2024-08-30 15:59:01 +0545

    feat: display nepali date and time (basic)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import NepaliDate from 'nepali-datetime'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [npDate, setNpDate] = useState('')
+  const [enDate, setEnDate] = useState('')
+
+  const [npHour, setNPHour] = useState('')
+  const [npMinute, setNPMinute] = useState('')
+  const [npSecond, setNPSecond] = useState('')
+  const [npAmPm, setNPAmPm] = useState('')
+
+  const updateTime = useCallback(() => {
+    const currentNepaliDateTime = new NepaliDate()
+    setNpDate(currentNepaliDateTime.formatNepali('MMMM DD, YYYY'))
+    setEnDate(dayjs().format('MMMM DD, YYYY'))
+
+    setNPHour(currentNepaliDateTime.format('hh'))
+    setNPMinute(currentNepaliDateTime.format('mm'))
+    setNPSecond(currentNepaliDateTime.format('ss'))
+    setNPAmPm(currentNepaliDateTime.format('A'))
+  }, [])
+
+  useEffect(() => {
+    updateTime()
+    const updateTimeInterval = setInterval(updateTime, 1000)
+    return () => clearInterval(updateTimeInterval)
+  }, [updateTime])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <div className="time-board">
+        <div className="font-montserrat english-date">
+          <span>{enDate}</span>
+        </div>
+        <div className="nepali-date font-mukta">
+          <span>{npDate}</span>
+        </div>
+        <div className="nepali-time font-montserrat">
+          <span className="nepali-time-sub nepali-hour">{npHour}</span>
+          <span className="nepali-time-sub nepali-minute">{npMinute}</span>
+          <span className="nepali-time-sub nepali-second">{npSecond}</span>
+          <span className="nepali-am-pm">{npAmPm}</span>
+        </div>
+      </div>
+      <div className="org-name font-montserrat">
+        Powered by{' '}
+        <a
+          target="_blank"
+          href="https://github.com/opensource-nepal/node-nepali-datetime"
+        >
+          node-nepali-datetime
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
